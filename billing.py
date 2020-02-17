df52cf8a371f2f6d11e8b2c3cc3d4a4da923e3c2
KAMESHMAX222 <61132907+KAMESHMAX222@users.noreply.github.com>
2020-02-17 14:44:25 +0530

    Create billing.py

---
+++ billing.py
@@ -0,0 +1,17 @@
+print("rj hotel")
+a=input("enter the name")
+print("1.veg","2.non veg")
+b=input("enter your choice:")
+if b in "1.veg":
+    print("1.veg")
+    print("1.dosa","2.idli","3.sappathi")
+    print("dosa=10Rs")
+    print("idli=3Rs")
+    print("sappathi=25Rs")
+    c=input("enter your choice")
+    print("1","2","3")
+    d=input("select the number of set")
+    print("your selection is successfull")
+    print("thank you welcome back again")      
+elif b in "2.non veg":
+    print("2.non veg")
