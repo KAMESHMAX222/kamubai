51b3019d2a5dfc3f1e4c5e82d5f3ba897d8fddd6
KAMESHMAX222 <61132907+KAMESHMAX222@users.noreply.github.com>
2020-02-17 15:33:28 +0530

    Update billing.py

--- billing.py
+++ billing.py
@@ -1,4 +1,4 @@
-print("rj hotel")
+print("pkhotel")
 a=input("enter the name")
 print("1.veg","2.non veg")
 b=input("enter your choice:")
@@ -12,6 +12,17 @@ if b in "1.veg":
     print("1","2","3")
     d=input("select the number of set")
     print("your selection is successfull")
-    print("thank you welcome back again")      
+    print("thank you "+a+" back again")      
 elif b in "2.non veg":
     print("2.non veg")
+    print("rice")
+    print("1.chicken","2.egg")
+    print("chicken rice=80rs")
+    print("egg rice=70rs")
+    e=input("enter your choice:")
+    print("1","2")
+    f=input("select number of set")
+    print("your selection is successfull")
+    print("thank you "+a+" come again")
+    
+
